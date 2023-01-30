355c44d2e5b5744a7e64578b9430de4a93e1bf90
pdoazan <paul.doazan@gmail.com>
2023-01-30 12:20:18 +0100

    Fix initDb

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -30,24 +30,24 @@ const User = UserModel(sequelize, DataTypes)
 
 const initDb = () => {
   return sequelize.sync().then(_ => {
-    pokemons.map(pokemon => {
-      Pokemon.create({
-        name: pokemon.name,
-        hp: pokemon.hp,
-        cp: pokemon.cp,
-        picture: pokemon.picture,
-        types: pokemon.types
-      }).then(pokemon => console.log(pokemon.toJSON()))
-    })
+    // pokemons.map(pokemon => {
+    //   Pokemon.create({
+    //     name: pokemon.name,
+    //     hp: pokemon.hp,
+    //     cp: pokemon.cp,
+    //     picture: pokemon.picture,
+    //     types: pokemon.types
+    //   }).then(pokemon => console.log(pokemon.toJSON()))
+    // })
 
-    bcrypt.hash('pikachu', 10)
-      .then(hash => {
-        User.create({
-          username: 'pikachu',
-          password: hash
-        })
-          .then(user => console.log(user.toJSON()))
-      })
+    // bcrypt.hash('pikachu', 10)
+    //   .then(hash => {
+    //     User.create({
+    //       username: 'pikachu',
+    //       password: hash
+    //     })
+    //       .then(user => console.log(user.toJSON()))
+    //   })
 
     console.log('La base de donnée a bien été initialisée !')
   })
